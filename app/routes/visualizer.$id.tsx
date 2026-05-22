f56668260eb53626839415a353ceec7e79b87892
Arpit <your@email.com>
2026-05-22 22:59:34 +0530

    host and upload images

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,26 +1,24 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import React from 'react'
+import { useLocation } from 'react-router'
 
-const Visualizer = () => {
-  const { id } = useParams();
-  const [base64Image, setBase64Image] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (!id) return;
-
-    const storageKey = `roometric-upload-${id}`;
-    setBase64Image(sessionStorage.getItem(storageKey));
-  }, [id]);
+const visualizerId = () => {
+  const location = useLocation();
+  const {initialImage, name} = location.state || {};
 
   return (
-    <div className="visualizer-route">
-      {base64Image ? (
-        <img src={base64Image} alt="Uploaded floor plan" />
-      ) : (
-        <p>No uploaded floor plan found.</p>
-      )}
-    </div>
+    <section>
+      <h1> {name || 'Untitled Project'}</h1>
+
+      <div className="visualizer">
+        {initialImage && (
+          <div className="image-container">
+            <h2>Source Image</h2>
+            <img src={initialImage} alt="source" />
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
-export default Visualizer
+export default visualizerId
